b350b475c5c043bacf465bed2f2d2517852d3153
Chris Keller <cjameskeller@gmail.com>
2017-04-08 13:28:11 -0500

    now favorites work but not in db yet

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,31 +2,51 @@ var myApp = angular.module("myApp", []);
 
 myApp.controller("FirstController", ["$scope", "FirstFactory", function($scope, FirstFactory){
   $scope.searchTitle = FirstFactory.searchTitle;
-  $scope.addMovie = FirstFactory.addMovie;
+  $scope.getMovie = FirstFactory.getMovie;
 
 }]);
 
 myApp.controller("SecondController", ["$scope", "FirstFactory", function($scope, FirstFactory){
-  $scope.movies = FirstFactory.allMovies.movies;
+  $scope.searchedMovie = FirstFactory.searchedMovie;
+  $scope.makeFavorite = FirstFactory.makeFavorite;
+  $scope.starredMovies = FirstFactory.favoriteMovies.starredMovies;
 
 }]);
 
 myApp.factory("FirstFactory", ["$http", function($http){
   var searchTitle = "";
-  var allMovies = {
-    movies: []
+
+  var searchedMovie = {};
+
+  var getMovie = function(name){
+    $http.get("http://www.omdbapi.com/?t=" + name + "&y=&plot=full&r=json").then(function(response){
+      console.log(response);
+      searchedMovie.name = response.data.Title;
+      searchedMovie.director = response.data.Director;
+      searchedMovie.genre = response.data.Genre;
+      searchedMovie.plot = response.data.Plot;
+      searchedMovie.poster = response.data.Poster;
+      searchedMovie.rated = response.data.Rated;
+      searchedMovie.released = response.data.Released;
+      searchedMovie.runtime = response.data.Runtime;
+      searchedMovie.exists = response.data.Response;
+      searchTitle = "";
+    });
   };
-  var addMovie = function(name){
-    var movie = {
-      name: name,
-    };
-    allMovies.movies.push(movie);
-    searchTitle = "";
+
+  var favoriteMovies = {
+    starredMovies: []
+  };
+
+  var makeFavorite = function(thisMovie){
+    favoriteMovies.starredMovies.push(thisMovie);
   };
 
   return{
     searchTitle: searchTitle,
-    allMovies: allMovies,
-    addMovie: addMovie
+    searchedMovie: searchedMovie,
+    getMovie: getMovie,
+    makeFavorite: makeFavorite,
+    favoriteMovies: favoriteMovies,
   };
 }]);

d6ca52b8ce0a51fe792bdbf1be18a1a83d2e0a5f
Chris Keller <cjameskeller@gmail.com>
2017-04-08 11:41:49 -0500

    works to store movie titles in array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,32 @@
 var myApp = angular.module("myApp", []);
+
+myApp.controller("FirstController", ["$scope", "FirstFactory", function($scope, FirstFactory){
+  $scope.searchTitle = FirstFactory.searchTitle;
+  $scope.addMovie = FirstFactory.addMovie;
+
+}]);
+
+myApp.controller("SecondController", ["$scope", "FirstFactory", function($scope, FirstFactory){
+  $scope.movies = FirstFactory.allMovies.movies;
+
+}]);
+
+myApp.factory("FirstFactory", ["$http", function($http){
+  var searchTitle = "";
+  var allMovies = {
+    movies: []
+  };
+  var addMovie = function(name){
+    var movie = {
+      name: name,
+    };
+    allMovies.movies.push(movie);
+    searchTitle = "";
+  };
+
+  return{
+    searchTitle: searchTitle,
+    allMovies: allMovies,
+    addMovie: addMovie
+  };
+}]);
